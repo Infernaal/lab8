057a6fb1e6adaa28215fbf9a498e91408da504e3
Roman <2014a3000lenovo@gmail.com>
2024-11-06 11:40:15 +0200

    Вирішення проблеми: Дивергентні зміни, Коментарі коду, Оператори перемикання

---
+++ src/main/java/org/example/AccountService.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class AccountService {
+
+    private final Account account;
+
+    public AccountService(Account account) {
+        this.account = account;
+    }
+
+    public void withdraw(Customer customer, double amount, String currency) {
+        if (!account.getCurrency().equals(currency)) {
+            throw new IllegalArgumentException("Can't withdraw in currency: " + currency);
+        }
+
+        double overdraftFee = calculateOverdraftFee(customer, amount);
+        account.setMoney(account.getMoney() - amount - overdraftFee);
+    }
+
+    private double calculateOverdraftFee(Customer customer, double amount) {
+        if (account.getMoney() >= 0) {
+            return 0;
+        }
+
+        double feeMultiplier = 1.0;
+
+        if (customer.getCustomerType() == CustomerType.COMPANY) {
+            feeMultiplier = account.getType().isPremium()
+                    ? customer.getCompanyOverdraftDiscount() / 2
+                    : customer.getCompanyOverdraftDiscount();
+        } else if (customer.getCustomerType() == CustomerType.PERSON && account.getType().isPremium()) {
+            feeMultiplier = 1.0;
+        }
+
+        return amount * account.overdraftFee() * feeMultiplier;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,102 +1,31 @@
 package org.example;
 
 public class Customer {
-
     private String name;
     private String surname;
     private String email;
     private CustomerType customerType;
-    private Account account;
-    private double companyOverdraftDiscount = 1;
+    private double companyOverdraftDiscount = 1.0;
 
-
-    public Customer(String name, String surname, String email, CustomerType customerType, Account account) {
+    public Customer(String name, String surname, String email, CustomerType customerType) {
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.customerType = customerType;
-        this.account = account;
     }
 
-    // use only to create companies
-    public Customer(String name, String email, Account account, double companyOverdraftDiscount) {
-        this.name = name;
-        this.email = email;
-        this.customerType = CustomerType.COMPANY;
-        this.account = account;
+    public Customer(String name, String email, double companyOverdraftDiscount) {
+        this(name, "", email, CustomerType.COMPANY);
         this.companyOverdraftDiscount = companyOverdraftDiscount;
     }
 
-    private String getFullName() {
-        return name + " " + surname;
-    }
+    public String getName() { return name; }
+    public String getSurname() { return surname; }
+    public String getEmail() { return email; }
+    public CustomerType getCustomerType() { return customerType; }
+    public double getCompanyOverdraftDiscount() { return companyOverdraftDiscount; }
 
-    public void withdraw(double amount, String currency) {
-        if (!account.getCurrency().equals(currency)) {
-            throw new RuntimeException("Can't withdraw in currency: " + currency);
-        }
-
-        double overdraftFee = calculateOverdraftFee(amount);
-        account.setMoney(account.getMoney() - amount - overdraftFee);
-    }
-
-    private double calculateOverdraftFee(double amount) {
-        if (account.getMoney() >= 0) {
-            return 0;
-        }
-
-        double feeMultiplier = 1.0;
-
-        if (customerType == CustomerType.COMPANY) {
-            feeMultiplier = account.getType().isPremium()
-                    ? companyOverdraftDiscount / 2
-                    : companyOverdraftDiscount;
-        } else if (customerType == CustomerType.PERSON && account.getType().isPremium()) {
-            feeMultiplier = 1.0;
-        }
-
-        return amount * account.overdraftFee() * feeMultiplier;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public CustomerType getCustomerType() {
-        return customerType;
-    }
-
-    public void setCustomerType(CustomerType customerType) {
-        this.customerType = customerType;
-    }
-
-    public String printCustomerDaysOverdrawn() {
-        String fullName = name + " " + surname + " ";
-        String accountDescription = "Account: IBAN: " + account.getIban() + ", Days Overdrawn: " + account.getDaysOverdrawn();
-        return fullName + accountDescription;
-    }
-
-    public String printCustomerMoney() {
-        String fullName = name + " " + surname + " ";
-        String accountDescription = "";
-        accountDescription += "Account: IBAN: " + account.getIban() + ", Money: " + account.getMoney();
-        return fullName + accountDescription;
-    }
-
-    public String printCustomerAccount() {
-        return "Account: IBAN: " + account.getIban() + ", Money: "
-                + account.getMoney() + ", Account type: " + account.getType();
+    public String getFullName() {
+        return name + (surname.isEmpty() ? "" : " " + surname);
     }
 }
