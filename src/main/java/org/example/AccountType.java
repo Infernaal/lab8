f18a96a91ca447533b33d174094f81481615f143
Roman <2014a3000lenovo@gmail.com>
2024-11-06 11:22:16 +0200

    Add new files java

---
+++ src/main/java/org/example/AccountType.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class AccountType {
+    private boolean premium;
+
+    public AccountType(boolean premium) {
+        this.premium = premium;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    @Override
+    public String toString() {
+        return premium ? "premium" : "normal";
+    }
+}
